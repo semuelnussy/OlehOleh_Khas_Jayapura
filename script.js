4397f3dd5d5682e2abdc2beb51c8df8a32d34a40
Semuel Iszak Nussy <88064470+semuelnussy@users.noreply.github.com>
2025-03-12 12:56:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart(item) {
+    alert(item + " telah ditambahkan ke keranjang!");
+}
+function kontakPerson() {
+    window.location.href = "https://wa.me/6281234567890?text=Halo,%20saya%20tertarik%20dengan%20menu%20Kafe%20Mama%20Primadona!";
+}
